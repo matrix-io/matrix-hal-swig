570e8662a6245ad0018048b68b991e02e827895a
Calos Chacin <chacin.carlos9798@gmail.com>
2019-03-25 14:47:25 -0400

    Fixed typo

--- drivers/info.cpp
+++ drivers/info.cpp
@@ -7,7 +7,7 @@ info::~info(){}
 // Determine if MATRIX is using bus or kernel modules
 bool info::isDirectBus(){
     // Kernel Modules are being used
-    if(!bus.IsDirectBus()) {return false};
+    if(!bus.IsDirectBus()) {return false;}
     // Bus is being used
-    else {return true};
+    else {return true;}
 }
